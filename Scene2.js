fa2305fd62bebfd945204b30c2fd3df4d14a237e
Mauri <linkinmjs@gmail.com>
2020-04-26 18:06:03 -0300

    Part 6 Physics

--- Scene2.js
+++ Scene2.js
@@ -53,6 +53,52 @@ class Scene2 extends Phaser.Scene{
       this.input.on('gameobjectdown', this.destroyShip, this);
 
       this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
+
+      // POWER UPS
+      this.anims.create({
+        key: "red",
+        frames: this.anims.generateFrameNumbers("power-up", {
+          start:0,
+          end:1
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+      this.anims.create({
+        key:"gray",
+        frames: this.anims.generateFrameNumbers("power-up",{
+          start: 2,
+          end: 3
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+
+      this.physics.world.setBoundsCollision();
+
+      this.powerUps = this.physics.add.group();
+      
+      var maxObjects = 4;
+      for (var i = 0; i <= maxObjects; i++) {
+        var powerUp = this.physics.add.sprite(16, 16, "power-up");
+        this.powerUps.add(powerUp);
+         powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+  
+        if (Math.random() > 0.5) {
+          powerUp.play("red");
+        } else {
+          powerUp.play("gray");
+        }
+  
+        powerUp.setVelocity(100, 100);
+        
+        powerUp.setCollideWorldBounds(true);
+        
+        powerUp.setBounce(1);
+  
+      }
+      
+
     }
 
     update(){
