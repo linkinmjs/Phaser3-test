e8c5b305da39de1c3b8f386b88b6cd6f194fbd5c
Mauri <linkinmjs@gmail.com>
2020-04-26 11:21:05 -0300

    Part 3 Game Objects Images

--- Scene1.js
+++ Scene1.js
@@ -1,10 +1,18 @@
 class Scene1 extends Phaser.Scene {
-    constructor(){
-        super('bootGame');
+    constructor() {
+        super("bootGame");
+      }
+
+    preload(){
+        //images
+        this.load.image("background","./assets/images/background.png");
+        this.load.image("ship1","./assets/images/ship.png");
+        this.load.image("ship2","./assets/images/ship2.png");
+        this.load.image("ship3","./assets/images/ship3.png");
     }
 
-    create(){
-        this.add.text(20,20, "Loading game...");
-        this.scene.start("playGame")
+    create() {
+        this.add.text(20, 20, "Loading game...");
+        this.scene.start("playGame");
     }
 }
--- game.js
+++ game.js
@@ -1,14 +1,10 @@
-window.onload = function(){
 
-    var config = {
-        width: 800,
-        height: 600,
-        backgroundColor: 0x000000,
-        scene: [Scene1, Scene2]
-    }
-
-
-    var game = new Phaser.Game(config);
-    
-
-}
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true
+  }
+  
+  var game = new Phaser.Game(config);
